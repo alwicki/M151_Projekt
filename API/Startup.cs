c1f7a9f2cfec363e8f63bb8ec40655a02f9eabae
Aurel <aurellinus.wicki@sluz.ch>
2021-05-14 23:41:33 +0200

    Create Db on startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,8 +40,9 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext dataContext)
         {
+            dataContext.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
