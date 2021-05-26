7a137b32446b49ce2f50ff2acede8d6c4e3cffcb
Aurel <aurellinus.wicki@sluz.ch>
2021-05-26 09:33:24 +0200

    Update UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,13 @@
 using API.Data;
 using API.Entities;
+using API.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace API.Controllers
 {
@@ -16,17 +21,48 @@ namespace API.Controllers
             this.context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers(){
+            return await this.context.Users.ToListAsync();
+        }
+
         [HttpGet("{id}")]
-        public ActionResult<User> GetUser(int id){
-            var user = this.context.Users.Find(id);
+        public async Task<ActionResult<User>> GetUser(int id){
+            return await this.context.Users.FindAsync(id);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username already exists");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new User{
+                UserName = registerDto.Username.ToLower(),
+                UserRole = 0,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            this.context.Users.Add(user);
+            await this.context.SaveChangesAsync();
+            
             return user;
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<User>> GetUsers(){
-            var users = this.context.Users.ToList();
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(LoginDto loginDto)
+        {
+            var user = await this.context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            
+            if(user == null) return Unauthorized("Invalid username or password");
 
-            return users;
+            //using var hmac = new HMACSHA512();
+        }
+
+        private async Task<bool> UserExists(string username){
+            return await this.context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
---
+++ API/Data/Migrations/20210519091115_UserPasswordHash.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPassword",
+                table: "Users");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "bytea",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "bytea",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPassword",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
